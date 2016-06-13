b15a5ac82da9c6ac13d4f55746bbd15771331d62
Hideaki SAWADA <chuno@live.jp>
2016-06-13 12:20:30 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -2,8 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hideaki SAWADA <chuno@live.jp>
  */
-$lang['current_pagename_tpl']  = '現在の名前空間のテンプレートページ名。このページが存在する場合、同じ名前空間に新規ページを作成すると、最初にその内容をエディタにロードする。';
-$lang['inherited_pagename_tpl'] = '上記通常の現在の名前空間のテンプレートファイルとして機能しますが、名前空間以下の全名前空間でも同様に使用される（継承される）。';
+$lang['current_pagename_tpl']  = '現在の名前空間のテンプレートページ名。このページが存在する場合、同じ名前空間に新しいページを作成した時、最初にその内容を編集画面に読み込みます。';
+$lang['inherited_pagename_tpl'] = '以下の全ての名前空間で使用される（継承される）という事を除いて、上記の現在の名前空間のテンプレートページ名ファイル同様に動作します。';
